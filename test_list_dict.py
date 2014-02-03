f4e657ecb4f01992e10f1359e2fef7f7a45c93f7
Toomore Chiang <toomore0929@gmail.com>
2014-02-04 01:31:32 +0800

    Add time result into `test_list_dict`.

--- test_list_dict.py
+++ test_list_dict.py
@@ -2,6 +2,7 @@ from timeit import timeit
 
 t = 'asdasdf;lkjalskdjfalwiefn.zcxn2345o;;adlskf'
 tt = dict.fromkeys(t)
+print tt
 ttt = set(t)
 
 def aa():
@@ -14,10 +15,13 @@ def cc():
     '6' in ttt
 
 print 'aa'
-print timeit(aa, number=10**10)
+print timeit(aa, number=10)
+# 8.10623168945e-06
 
 print 'bb'
-print timeit(bb, number=10**10)
+print timeit(bb, number=10)
+# 5.00679016113e-06
 
 print 'cc'
-print timeit(cc, number=10**10)
+print timeit(cc, number=10)
+# 5.00679016113e-06
