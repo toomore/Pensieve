e82182cb42924102102e891286f2b39a8e6c13d1
Toomore Chiang <toomore0929@gmail.com>
2014-02-04 01:26:52 +0800

    Add time resule into `test_list_and_tuple`.

--- test_list_and_tuple.py
+++ test_list_and_tuple.py
@@ -2,17 +2,19 @@ from timeit import timeit
 
 
 def aa():
-    a = [i for i in range(10)]
+    a = [i for i in range(1000)]
     for i in a:
         i
 
 def bb():
-    b = (i for i in range(10))
+    b = (i for i in range(1000))
     for i in b:
         i
 
 print 'aa'
 print timeit(aa, number=10)*1000000
+# 900.030136108
 
 print 'bb'
 print timeit(bb, number=10)*1000000
+# 1386.1656189
