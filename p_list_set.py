b522bce67b4fdecf7bb196a566f48d5a6e3945fc
Toomore Chiang <toomore0929@gmail.com>
2014-02-03 12:53:42 +0800

    Add time result into `p_list_set`

--- p_list_set.py
+++ p_list_set.py
@@ -1,16 +1,25 @@
 from timeit import timeit
 
-a = [1, 2, 3, 4]
+a = range(100)
 b = set(a)
 
 m = min(a)
 x = max(a)
 
 def aa():
-    4 in a
+    100 in a
 
 def bb():
-    m >= 4 >= x
+    100 in b
+
+def cc():
+    m >= 100 >= x
 
 print timeit(aa)
+# 2.40269112587
+
 print timeit(bb)
+# 0.211581945419
+
+print timeit(cc)
+# 0.235991001129
