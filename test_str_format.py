06123e5971761eeefb08c946f68df504f14e3b43
Toomore Chiang <toomore0929@gmail.com>
2014-02-04 01:45:21 +0800

    Add time result into `test_str_format`.

--- test_str_format.py
+++ test_str_format.py
@@ -2,6 +2,7 @@ from timeit import timeit
 
 def aa():
     a = '%s%s%s' % (1, 2, 3)
+    assert a == '123'
 
 def aaa():
     a = '%(a)s%(b)s%(c)s' % ({'a': 1, 'b': 2, 'c': 3})
@@ -9,6 +10,7 @@ def aaa():
 
 def bb():
     b = '{0}{1}{2}'.format(1, 2, 3)
+    assert b == '123'
 
 def bbb():
     b = '{a}{b}{c}'.format(**{'a': 1, 'b': 2, 'c': 3})
@@ -16,8 +18,12 @@ def bbb():
 
 print 'str'
 print timeit(aa)
+# 0.524276971817
 print timeit(aaa)
+# 0.849791049957
 
 print 'format'
 print timeit(bb)
+# 0.865188121796
 print timeit(bbb)
+# 1.11158490181
