7e5a7e213d9dfe332f2faa58a9a442430efa9f23
Toomore Chiang <toomore0929@gmail.com>
2014-02-06 02:13:04 +0800

    Class MRO advance.

--- class_mro.py
+++ class_mro.py
@@ -24,3 +24,36 @@ c = C()
 print super(C, c).a == 2
 print super(A, c).a == 2
 print super(B, c).a == 0
+
+class A(object):
+    a = 'A'
+
+class B(object):
+    a = 'B'
+
+class C(object):
+    a = 'C'
+
+class D(object):
+    a = 'D'
+
+class E(object):
+    a = 'E'
+
+class K1(A, B, C): pass
+class K2(D, B, E): pass
+class K3(D, A): pass
+class Z(K1, K2, K3): pass
+
+# L[Z]: Z K1 K2 K3 D A B C E O
+
+z = Z()
+
+print super(Z, z).a == 'D'
+print super(K1, z).a == 'D'
+print super(K2, z).a == 'D'
+print super(K3, z).a == 'D'
+print super(D, z).a == 'A'
+print super(A, z).a == 'B'
+print super(B, z).a == 'C'
+print super(C, z).a == 'E'
