37d06d57d0fccdc1c3e688d837c4a4f8edf70747
Toomore Chiang <toomore0929@gmail.com>
2014-02-09 13:11:36 +0800

    Using property.

--- class_descriptor.py
+++ class_descriptor.py
@@ -2,14 +2,22 @@
 # http://docs.python.org/2/reference/datamodel.html#implementing-descriptors
 # TODO: implement it.
 
+
 class aaa(object):
     def __init__(self):
         print 'in init'
-        self._x = {}
+        self._x = 0
 
-    def __get__(self, name):
-        return name
+    def __get__(self):
+        return self._x
+
+    def __set__(self, value):
+        self._x = value
+
+    x = property(__get__, __set__)
 
 if __name__ == '__main__':
     a = aaa()
-    print a.get('123')
+    print a.x
+    a.x = 123
+    print a.x
