1d6f64520288dcbb8e2ba99b75b7931fee3991b1
Toomore Chiang <toomore0929@gmail.com>
2014-02-13 02:27:47 +0800

    Add string conversion.

--- expressions.py
+++ expressions.py
@@ -4,3 +4,11 @@
 print {i:i for i in xrange(10)}
 print (i for i in xrange(10))
 print {i for i in xrange(10)}
+
+# ----- String conversions ----- #
+print `1, 2, 3`
+
+c = '3'
+print `1, 2, c`
+
+#print [yield i for i in range(10)]
