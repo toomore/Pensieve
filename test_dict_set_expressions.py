deb8ddeecd484249051fe0c5b8e9d2a3f77cfa9c
Toomore Chiang <toomore0929@gmail.com>
2014-02-14 09:20:35 +0000

    Tiny changed test list, set, tuple expressions.

--- test_dict_set_expressions.py
+++ test_dict_set_expressions.py
@@ -3,14 +3,23 @@ from timeit import timeit
 
 
 def aa():
+    ''' list '''
     a = [i for i in xrange(10)]
-    a = set(a)
+    #a = set(a) # 1.99305200577
 
 def bb():
+    ''' set '''
     a = {i for i in xrange(10)}
+
+def cc():
+    ''' tuple '''
+    a = (i for i in xrange(10))
+    #a = set(a) # 1.60462594032
 
 if __name__ == '__main__':
    print timeit(aa)
-   # 1.99305200577
+   # 1.09882903099
    print timeit(bb)
-   # 1.60462594032
+   # 1.67259907722
+   print timeit(cc)
+   # 1.00920009613
