4080ae855cdcdd42462074368fd54f54251c96ca
Toomore Chiang <toomore0929@gmail.com>
2014-02-16 12:19:59 +0800

    Add time_mark repr.

--- time_mark.py
+++ time_mark.py
@@ -7,6 +7,16 @@ class timemark(object):
     def __init__(self, target):
         self.target = target
         self._type = type(target)
+        self.time = self._time()
+
+    def __repr__(self):
+        return '%s %s(%s)' % (self.time, self.target, self._type)
 
     def _time(self):
         return timeit(self.target)
+
+if __name__ == '__main__':
+    def aa():
+        [i for i in xrange(10)]
+
+    print timemark(aa)
