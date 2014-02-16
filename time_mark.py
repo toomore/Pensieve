dc9753f55f157ddf36fb9b525ae9ab85ad328c61
Toomore Chiang <toomore0929@gmail.com>
2014-02-16 12:37:09 +0800

    Add number.

--- time_mark.py
+++ time_mark.py
@@ -4,17 +4,18 @@ from timeit import timeit
 
 class timemark(object):
 
-    def __init__(self, target):
+    def __init__(self, target, number=None):
         self.target = target
         self._type = type(target)
-        self.time = self._time()
+        self.time = self._time(number)
 
     def __repr__(self):
         return '%s %s(%s)' % (self.time, self.target.__name__,
                               self._type.__name__)
 
-    def _time(self):
-        return timeit(self.target)
+    def _time(self, number):
+        number = number or 1000000
+        return timeit(self.target, number=number)
 
 if __name__ == '__main__':
     def aa():
