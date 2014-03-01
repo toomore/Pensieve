31d42b55ed38815ec5a86ebb60c29ffd0099f5cb
Toomore Chiang <toomore0929@gmail.com>
2014-03-01 17:30:14 +0800

    Add param into class_new_init.

--- class_new_init.py
+++ class_new_init.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
 
 class aa(object):
-    pass
+    def __init__(self):
+        print 'in aa init'
+
+    def name(self):
+        return 'toomore'
+
 
 class tt(object):
     def __new__(self, *args, **kwargs):
         print 'in new'
-        return type('toomore', (int,), {'name': 123})
+        print args, kwargs
+        return type('toomore', (aa,), {'age': 123})()
 
     def __init__(self):
         print 'in init'
 
 if __name__ == '__main__':
-    t = tt()
+    t = tt(1, 2)
     help(t)
+    print t.name()
+    print t.age
