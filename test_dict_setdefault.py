1c1c853c82aa68f03de95cdad1c4d1feb7bbb8f8
Toomore Chiang <toomore0929@gmail.com>
2014-03-06 13:18:23 +0800

    Add time result.

--- test_dict_setdefault.py
+++ test_dict_setdefault.py
@@ -21,6 +21,10 @@ def dd():
 
 if __name__ == '__main__':
     print timeit(aa)
+    # 0.486865997314
     print timeit(bb)
+    # 0.369936943054
     print timeit(cc)
+    # 0.307506084442
     print timeit(dd)
+    # 0.338114023209
