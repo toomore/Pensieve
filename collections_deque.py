a1b4f001852d899b6ed6981bac74c28e74fa8aaa
Toomore Chiang <toomore0929@gmail.com>
2014-03-18 00:21:35 +0800

    Add class to test get global queue.

--- collections_deque.py
+++ collections_deque.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
 from collections import deque
+from timeit import timeit
 
 SAMPLE = deque(range(10))
 
-def aa():
-    q = SAMPLE.pop()
-    SAMPLE.appendleft(q)
-    return q
+class aa(object):
+    @staticmethod
+    def get_queue():
+        q = SAMPLE.pop()
+        SAMPLE.appendleft(q)
+        return q
 
-def bb():
-    q = SAMPLE.pop()
-    SAMPLE.appendleft(q)
-    return q
+class bb(object):
+    @staticmethod
+    def get_queue():
+        q = SAMPLE.pop()
+        SAMPLE.appendleft(q)
+        return q
 
 for i in range(10):
-    print u'aa', aa()
-    print u'bb', bb()
+    #print u'aa', aa.get_queue()
+    #print u'bb', bb.get_queue()
+    print timeit(aa.get_queue)
+    print timeit(bb.get_queue)
