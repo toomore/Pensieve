0ab90d8f86231756617e8524708583f0a4cd1786
Toomore Chiang <toomore0929@gmail.com>
2014-03-19 10:26:28 +0800

    Add time result.

--- test_collections_deque_list_insert.py
+++ test_collections_deque_list_insert.py
@@ -2,24 +2,27 @@
 from timeit import timeit
 from collections import deque
 
-AA_SAMPLE = deque(range(100))
+n = 100
+
+AA_SAMPLE = deque(range(n))
 def aa():
     get_one = AA_SAMPLE.pop()
     AA_SAMPLE.appendleft(get_one)
 
-BB_SAMPLE = range(100)
+BB_SAMPLE = range(n)
 def bb():
     get_one = BB_SAMPLE.pop()
     BB_SAMPLE.insert(0, get_one)
 
-CC_SAMPLE = deque(range(100))
+CC_SAMPLE = deque(range(n))
 def cc():
     get_one = CC_SAMPLE[-1]
     CC_SAMPLE.rotate(1)
 
 if __name__ == '__main__':
     print timeit(aa), len(AA_SAMPLE)
-    # 0.372648954391
+    # 0.431893110275
     print timeit(bb), len(BB_SAMPLE)
-    # 0.743675947189
+    # 0.762758016586
     print timeit(cc), len(CC_SAMPLE)
+    # 0.561318159103
