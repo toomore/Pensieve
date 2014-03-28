7dc4cc02309bde63a98163b9ae55929474d9d818
Toomore Chiang <toomore0929@gmail.com>
2014-03-28 15:11:22 +0800

    Tiny changed in test_str_decode.

--- test_str_decode.py
+++ test_str_decode.py
@@ -5,19 +5,20 @@ from timeit import timeit
 STR_SAMPLE = '國'
 
 def aa():
-    STR_SAMPLE.decode('UTF-8')
+    return STR_SAMPLE.decode('UTF-8')
 
 def bb():
-    STR_SAMPLE.decode('utf-8')
+    return STR_SAMPLE.decode('utf-8')
 
 def cc():
-    STR_SAMPLE.decode('UTF8')
+    return STR_SAMPLE.decode('UTF8')
 
 def dd():
-    STR_SAMPLE.decode('utf8')
+    return STR_SAMPLE.decode('utf8')
 
 if __name__ == '__main__':
-    print timeit(aa)
-    print timeit(bb)
-    print timeit(cc)
-    print timeit(dd)
+    if aa() == bb() == cc() == dd():
+        print timeit(aa)
+        print timeit(bb)
+        print timeit(cc)
+        print timeit(dd)
