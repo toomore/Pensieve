79a7fa440208df0f42bafaf17cababff14d0a9ca
Toomore Chiang <toomore0929@gmail.com>
2014-04-14 23:27:33 +0800

    Tiny changed.

--- test_type_int_float.py
+++ test_type_int_float.py
@@ -2,7 +2,7 @@
 from timeit import timeit
 
 
-def covstr():
+def covstr(strings):
     """ convert string to int or float. """
     try:
         result = int(strings)
@@ -10,11 +10,23 @@ def covstr():
         result = float(strings)
     return result
 
-def aa():
+def aa(strings):
     return float(strings) if '.' in strings else int(strings)
 
+def CC_A():
+    covstr(1)
+
+def CC_B():
+    convert(1.1)
+
+def DD_A():
+    aa(1)
+
+def DD_B():
+    aa(1.1)
+
 if __name__ == '__main__':
-    print timeit(covstr, setup='strings=1')
-    print timeit(aa, setup='strings=1')
-    print timeit(covstr, setup='strings=1.1')
-    print timeit(aa, setup='strings=1.1')
+    print timeit(CC_A)
+    print timeit(CC_B)
+    print timeit(DD_A)
+    print timeit(DD_B)
