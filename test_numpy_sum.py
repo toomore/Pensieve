20c08d3481c84ce7393db2840454f072e71a05af
Toomore Chiang <toomore0929@gmail.com>
2014-05-21 08:57:52 +0800

    Add test_numpy_sum.

---
+++ test_numpy_sum.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from numpy import array
+from numpy import sum as numpy_sum
+from timeit import timeit
+
+SAMPLE = range(1, 100)
+SAMPLE_2 = array(SAMPLE)
+def aa():
+    sum(SAMPLE)
+
+def bb():
+    numpy_sum(SAMPLE_2)
+
+if __name__ == '__main__':
+    print timeit(aa)
+    print timeit(bb)
