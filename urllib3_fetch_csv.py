f9fb367faea20d89c936445b070afb4756b14413
Toomore Chiang <toomore0929@gmail.com>
2014-06-07 23:12:11 +0800

    Add make_headers.

--- urllib3_fetch_csv.py
+++ urllib3_fetch_csv.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from cStringIO import StringIO
-from contextlib import closing
 import csv
 import urllib3
+from cStringIO import StringIO
+from contextlib import closing
+from urllib3.util.request import make_headers
 
 
 conn = urllib3.connection_from_url('http://s3.toomore.net/')
@@ -12,3 +13,5 @@ with closing(StringIO(result.data)) as files:
     csv_files = csv.reader(files)
     for i in csv_files:
         print i
+
+print make_headers(keep_alive=True)
