24078b7e34dc7bf9e9f4216d63bc4b564eb223c8
Toomore Chiang <toomore0929@gmail.com>
2014-06-08 23:50:02 +0800

    Tiny changed.

--- urllib3_PoolManager.py
+++ urllib3_PoolManager.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import urllib3
 
-http = urllib3.PoolManager()
+http = urllib3.PoolManager(5)
 result = http.request('GET', 'http://www.pinkoi.com/')
 print result.status
 print result.headers
