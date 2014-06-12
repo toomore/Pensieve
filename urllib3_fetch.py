db92eba6b2c56f4e177d519222eeb45e0dfbd4e0
Toomore Chiang <toomore0929@gmail.com>
2014-06-12 08:01:16 +0800

    Tiny changed.

--- urllib3_fetch.py
+++ urllib3_fetch.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 import urllib3
 
+
 class Urlfetch(object):
     def __init__(self, base_url):
         self.conn = urllib3.connection_from_url(base_url)
 
-    def get(self, path, params=None):
-        return self.conn.request('GET', path)
+    def get(self, path, params=None, headers=None):
+        return self._request('GET', path, fields=params, headers=headers)
 
-    def post(self, path, data=None):
-        return self.conn.request('POST', path)
+    def post(self, path, data=None, headers=None):
+        return self._request('POST', path, fields=data, headers=headers)
+
+    def _request(self, method, path, fields=None, headers=None):
+        return self.conn.request(method, path, fields=fields, headers=headers)
 
 if __name__ == '__main__':
-    conn = Urlfetch('http://www.pinkoi.com/')
-    print conn.get('/offers').data
-
+    conn = Urlfetch('http://httpbin.org/')
+    print conn.get('/get', {'name': 'Toomore'}).data
+    print conn.post('/post', {'name': 'Toomore'}).data
