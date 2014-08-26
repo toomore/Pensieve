063f3d91559e5f34b65db9424d9a08df49ff7f70
Toomore Chiang <toomore0929@gmail.com>
2014-08-26 15:44:17 +0000

    Add get columns names.

--- MariaDB/select.py
+++ MariaDB/select.py
@@ -6,9 +6,13 @@ import setting
 conn = MySQLdb.connect('localhost', setting.DBUSER, setting.DBPASS, setting.DBS)
 cur = conn.cursor()
 cur.execute('select * from pootlecache26 order by expires desc limit 10')
-print cur, dir(cur)
 
 result = cur.fetchall()
 
 for i in result:
     print i
+
+print cur, dir(cur)
+columns = [i[0] for i in cur.description]
+print columns
+cur.close()
