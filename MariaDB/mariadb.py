ef361c3290efbced98b4a6aff54f2f746f18e651
Toomore Chiang <toomore0929@gmail.com>
2014-11-15 10:27:32 +0000

    Add `get_columns`.

--- MariaDB/mariadb.py
+++ MariaDB/mariadb.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+'''
+ref: https://www.python.org/dev/peps/pep-0249/
+'''
 import MySQLdb
 import setting
 
@@ -6,18 +9,28 @@ import setting
 class MariaDB(object):
 
     def __init__(self):
-        conn = MySQLdb.connect('localhost', setting.DBUSER, setting.DBPASS, setting.DBS)
-        self.cur = conn.cursor()
+        self.conn = MySQLdb.connect('localhost', setting.DBUSER, setting.DBPASS, setting.DBS)
+        self.cur = self.conn.cursor()
 
     def __enter__(self):
+        print 'ENTERRR'
         return self
 
     def sql(self, sql):
         self.cur.execute(sql)
         return self.cur.fetchall()
 
+    def get_columns(self):
+        return [i[0] for i in self.cur.description]
+
+    def get_cur(self):
+        return self.cur
+
     def __exit__(self, type, value, traceback):
+        print type, value, traceback
         self.cur.close()
+        self.conn.close()
+
 
 class Userdata(MariaDB):
     pass
@@ -29,6 +42,9 @@ with Userdata() as userdata:
 
 for i in result:
     print i
+
+print userdata.get_columns()
+print 'get cur:', userdata.get_cur().rowcount
 
 #print '>>> print cur', cur, dir(cur)
 #columns = [i[0] for i in cur.description]
