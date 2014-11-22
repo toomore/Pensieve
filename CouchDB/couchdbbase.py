ba1337a3cef88eb67ea7dc1bdfb26c1d4747aeba
Toomore Chiang <toomore0929@gmail.com>
2014-11-22 15:17:10 +0000

    Add simple save.

--- CouchDB/couchdbbase.py
+++ CouchDB/couchdbbase.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
 import couchdb
 
+COUCHDB = couchdb.Server()
 
 class CouchDBBase(dict):
 
-    def __init__(self, db_name):
+    def __init__(self, db_name, _id):
         self.db_name = db_name
+        self._id = _id
 
     def save(self):
-        print self
+        data = COUCHDB[self.db_name].get(self._id, {'_id': self._id})
+        data.update(self)
+        COUCHDB[self.db_name].save(data)
 
 
 class Test(CouchDBBase):
     db_name = 'toomoretest'
 
     def __init__(self, _id):
-        super(Test, self).__init__(self.db_name)
+        super(Test, self).__init__(self.db_name, _id)
 
 
 if __name__ == '__main__':
-    test = Test('op')
-    test['name'] = 'toomore'
+    test = Test('oppp')
+    test['name'] = 'toomore2'
     test.save()
